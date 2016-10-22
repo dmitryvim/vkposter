d101c2d8d98f51c2b9de96e6cfab6c99c1f9432c
dmitryvim <dmitry.mikhailovich@gmail.com>
2016-10-22 14:15:39 +0300

    start spring boot application

---
+++ src/main/java/com/dmitryvim/vkposter/VkposterApplication.java
@@ -0,0 +1,16 @@
+package com.dmitryvim.vkposter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring boot application runner with comfiguration
+ * <p>
+ * dmitry.mikhailovich@gmail.com
+ */
+@SpringBootApplication
+public class VkposterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VkposterApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dmitryvim/vkposter/application/VkposterApplicationService.java
@@ -0,0 +1,12 @@
+package com.dmitryvim.vkposter.application;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Application service for commands
+ * <p>
+ * dmitry.mikhailovich@gmail.com
+ */
+@Service
+public class VkposterApplicationService {
+}
